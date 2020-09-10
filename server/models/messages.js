3ff125b7b724a04aa2d6591e45b52c4764613705
Theodore Nguyen <theodorenguyen5@gmail.com>
2020-09-09 20:52:09 -0700

    Created two tables and added a record

--- server/models/messages.js
+++ server/models/messages.js
@@ -1,6 +1,20 @@
 var db = require('../db');
 
+// var connection = mysql.;
+
+
 module.exports = {
-  getAll: function () {}, // a function which produces all the messages
+  getAll: function () {
+    console.log("We Ran the request!!!")
+    db.query("SELECT * FROM messages", (err, result) => {
+      if (err) {
+        console.error('error getting messages: ' + error.stack);
+      } else {
+        console.log(result);
+      }
+    })
+  }, // a function which produces all the messages
   create: function () {} // a function which can be used to insert a message into the database
 };
+
+module.exports.getAll();
--- server/schema.sql
+++ server/schema.sql
@@ -4,10 +4,18 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  text VARCHAR(175),
+  user_id INT NOT NULL,
+  PRIMARY KEY(id)
 );
 
 /* Create other tables and define schemas for them here! */
-
+CREATE TABLE user (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_name VARCHAR (25),
+  PRIMARY KEY(id)
+)
 
 
 
